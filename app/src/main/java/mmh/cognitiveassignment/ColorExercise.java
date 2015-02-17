3a5b6976e05fa971e9236ebc063a09535601f09a
josephine-bob <josephine.robert@eleves.ec-nantes.fr>
2015-02-18 00:41:52 +0100

    v1

---
+++ app/src/main/java/mmh/cognitiveassignment/ColorExercise.java
@@ -0,0 +1,15 @@
+package mmh.cognitiveassignment;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+
+public class ColorExercise extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_color_exercise);
+    }
+
+}
--- app/src/main/java/mmh/cognitiveassignment/MainActivity.java
+++ app/src/main/java/mmh/cognitiveassignment/MainActivity.java
@@ -4,7 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.content.Intent;
+import android.view.View;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -13,7 +14,6 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,5 +35,9 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void launchExercise(View view) {
+        startActivity(new Intent(MainActivity.this, ColorExercise.class));
     }
 }

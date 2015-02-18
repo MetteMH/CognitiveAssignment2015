421f59beb3a3ed10a75557fb6a1768d9ffbace5e
josephine-bob <josephine.robert@eleves.ec-nantes.fr>
2015-02-18 11:25:31 +0100

    Prototype working

--- app/src/main/java/mmh/cognitiveassignment/ColorExercise.java
+++ app/src/main/java/mmh/cognitiveassignment/ColorExercise.java
@@ -1,15 +1,33 @@
 package mmh.cognitiveassignment;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.graphics.Color;
+import java.util.Random;
+import java.lang.*;
+import android.os.SystemClock;
 
 
 public class ColorExercise extends ActionBarActivity {
+
+    private static final int num_colors = 5;
+    private Random mRandom = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color_exercise);
+/**        View v = findViewById(R.id.colorExercise);
+        SystemClock.sleep(3000);
+        v.setBackgroundColor(getResources().getColor(R.color.blue));
+        SystemClock.sleep(3000);
+        startActivity(new Intent(ColorExercise.this, Feedback.class));**/
+    }
+
+    public void getFeedback(View view) {
+        startActivity(new Intent(ColorExercise.this, Feedback.class));
     }
 
 }
